8fff81b8fc675a0489ec3f7441ab2db1e9dbe38b
vessleF21 <2022990058@gzhmu.edu.cn>
2025-09-06 13:10:02 +0800

    Rename fig 4b.R to fig 5b.R

--- Figure_code/Fig 4/fig 4b.R
+++
@@ -1,3 +0,0 @@
-
-
-This figure was created using Adobe Illustrator and the LocusZoom web tool (http://locuszoom.org/).
---
+++ Figure_code/Fig 5/fig 5b.R
@@ -0,0 +1,3 @@
+
+
+This figure was created using Adobe Illustrator and the LocusZoom web tool (http://locuszoom.org/).
